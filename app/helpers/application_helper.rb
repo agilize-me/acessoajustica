7c53db848e3870ba0f92ac61bd7bf79348d519db
BarbaraTieko <barbara_tieko@hotmail.com>
2017-04-11 19:12:38 -0300

    Change logo and site color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def page_title
-    title = "Acesso à Justiça"
+    title = "Parajás"
     title << " (#{Tenant.current.nome}) " if Tenant.current.present?
     title
   end
